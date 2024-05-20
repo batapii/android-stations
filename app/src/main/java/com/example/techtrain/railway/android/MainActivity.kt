abd143aec496f923f5dbf5abb137c31b531ee25c
taishi <batapi3939@gmail.com>
2024-05-20 15:27:34 +0900

    S09

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.techtrain.railway.android
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.text).text = getString(R.string.text)
+        val text = findViewById<TextView>(R.id.text)
+        val button = findViewById<Button>(R.id.button)
+        val EditText = findViewById<EditText>(R.id.editTextText)
+
+        button.setOnClickListener {
+            text.text = EditText.text
+        }
     }
 }
