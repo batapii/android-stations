b05d7d44e4d38f35215bc7aab903d6325071c5bd
taishi <batapi3939@gmail.com>
2024-05-20 15:07:08 +0900

    S08

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         findViewById<TextView>(R.id.text).text = getString(R.string.text)
     }
 }
