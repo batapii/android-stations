a46a605377012ab03f1e8d93d2899f3b8382f39d
taishi <batapi3939@gmail.com>
2024-05-23 10:37:01 +0900

    S13

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.techtrain.railway.android
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.techtrain.railway.android.databinding.ActivityMainBinding
 
@@ -14,14 +16,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
-
+        Log.d(TAG, "onCreate()")
         binding.button.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java).apply {
                 putExtra("KEY_INPUT_TEXT", binding.editTextText.text.toString())
             }
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "MainActivity")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "MainActivity")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "MainActivity")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "MainActivity")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "MainActivity")
     }
 }
